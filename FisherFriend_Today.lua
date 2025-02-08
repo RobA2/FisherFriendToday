c558cda393906acd6c8175b75fd3d0e01fd1cd88
RobA2 <aceaxe11@gmail.com>
2025-02-08 12:03:42 -0700

    Add files via upload

--- FisherFriend_Today.lua
+++ FisherFriend_Today.lua
@@ -27,10 +27,21 @@ local fftpin={
 	"619:4468:6197",--margoss
 }
 --todo list: combine way & pin but use each npc as separate tables?
+
 SLASH_FFT1 = "/fft"
 --note xxx=yyy vs xxx=(yyy) -- save the function vs save the result of the function
 local function fftmain(opt)--add 1-6 adj--can use multi opt? /fft xxx xxx
-	local adj=0--set as a dropdown list for any possible timing issues
+	--	opt=format("%w",opt) --trying to limit # of parameters didnt work lol
+	local adj=0 --future plan-set as a dropdown list if cycle is out of sync with server
+	--if not opt=='' then
+	if strmatch(opt,"%d") then
+		adj=tonumber(opt)
+	end
+	
+	--if strmatch(opt[2],"%d") then -- this will all be in a controlled option menu
+	--	adj=tonumber(opt[2]) -- so stop fiddling with command line parameters lol
+	--end
+	--end
 	--local ft=#fftbl--table length catcher--if needed in future
 	local t=tonumber(C_DateAndTime.GetServerTimeLocal())
 	local qrt=GetQuestResetTime() --must be a variable
@@ -45,16 +56,17 @@ local function fftmain(opt)--add 1-6 adj--can use multi opt? /fft xxx xxx
 	if opt == 'n' or opt=='next' then ff=fn end -- added for pin next
 	local usepin=("|cffffff00|Hworldmap:"..fftpin[ff].."|h[|A:Waypoint-MapPin-ChatIcon:13:13:0:0|a "..fftbl[ff].."]|h|r")
 	--start slash command processing
-	if opt=='' then
-		print(" ")
-		print("FF Today: "..fftbl[ff]..". Reset ["..art.."] in "..qrts);
+	if opt=='' or adj>0 then
+		if adj>0 then print("|cffddaaffFF Today: [adjustment="..adj.."]|r")
+		else print("|cffddaaffFF Today:|r") end
+		print("|cffddddff "..fftbl[ff]..". Reset ["..art.."] in "..qrts.."|r");
 	end
 	if opt=='p' or opt=='pin' then
 		--save next note for table redo list
 		--C_Map.SetUserWaypoint(UiMapPoint.CreateFromVector2D(fftpin[1],CreateVector2D(fftpin[2],fftpin[3])))
 		DEFAULT_CHAT_FRAME:AddMessage(usepin);
 	end
-	if opt == 'w' or opt=='way'  then
+	if opt=='w' or opt=='way'  then
 		if ttcheck then
 			SlashCmdList.TOMTOM_WAY(fftway[ff]..fftbl[ff])
 		else
@@ -65,28 +77,29 @@ local function fftmain(opt)--add 1-6 adj--can use multi opt? /fft xxx xxx
 		if ttcheck then
 			SlashCmdList.TOMTOM_WAY(fftway[fn]..fftbl[fn])
 		else
-			DEFAULT_CHAT_FRAME:AddMessage(usepin);--ff=fn
+			DEFAULT_CHAT_FRAME:AddMessage(usepin);-- ff=fn
 		end
 	end
-	if opt == 'm' or opt == 'mar' then
+	if opt=='m' or opt=='mar' then
 		if ttcheck then
 			SlashCmdList.TOMTOM_WAY(fftway[7]..fftbl[7])
 		else
 			DEFAULT_CHAT_FRAME:AddMessage(usepin);--added opt7 check to show margoss usepin
 		end
 	end
-	if opt == '?' or opt=='help' then
-		print(" ")
+	if opt=='?' or opt=='help' then
 		if not ttcheck then
-			print("|cffff8800**[TomTom] not detected-Using map pins**|r")
+			print("|cffff8800**[TomTom] not detected-FFT will use map pins**|r")
+		else
+			print("|cffccffcc                 ---FFT---|r")
 		end
-		print("|cffffcccc/fft|r -prints the current Fisherfriend and reset time")
-		print("|cffffcccc/fft p / pin|r -map pin link for current Fisherfriend")
-		print("|cffffcccc/fft w / way|r -set waypoint for current Fisherfriend")
-		print("|cffffcccc/fft n / next|r -set waypoint for the next Fisherfriend")
-		print("|cffffcccc/fft m / mar|r -set waypoint for Margoss")
-		--print("   [though he is not a FisherFriend, is related enough :)")
-		print("|cffffcccc/fft ? or /fft help|r -this help list :)")
+		print("|cffffcccc/fft|r -prints the current Fisherfriend and reset time|r")
+		print("|cffffcccc/fft p / pin|r -map pin link for current Fisherfriend|r")
+		print("|cffffcccc/fft w / way|r -set waypoint for current Fisherfriend|r")
+		print("|cffffcccc/fft n / next|r -set waypoint for the next Fisherfriend|r")
+		print("|cffffcccc/fft m / mar|r -set waypoint for Margoss|r")
+		print("|cffffcc88/fft 1-5  -adjustment value if cycle is out of sync|r")
+		--print("|cffffcccc/fft ? or /fft help|r -this help list :)|r")
 		print("|Cffff88ff/rl       -Reload interface|r")
 	end
 	if opt== 'info' then
@@ -101,8 +114,21 @@ local function fftmain(opt)--add 1-6 adj--can use multi opt? /fft xxx xxx
 		local month = CALENDAR_FULLDATE_MONTH_NAMES[d.month]
 		print(format("Realm time is %02d:%02d, %s, %d %s %d", d.hour, d.minute, weekDay, d.monthDay, month, d.year))
 	end
+	if opt=='a' then
+		RaidNotice_AddMessage(RaidWarningFrame,"FF Today: "..fftbl[ff]..". Reset ["..art.."]",ChatTypeInfo["RAID_WARNING"]);
+	end
 end
 	SlashCmdList["FFT"] = fftmain
 	SLASH_RL1 = "/rl"
 	SlashCmdList["RL"] = function() ReloadUI() end
+--run once for announcement
+C_Timer.After(0, function() -- leave at 0
+	C_Timer.After(3, function() -- default is 3... 
+		-- can set for whatever.. if 30, will run announcement after 30 seconds regardless of other addons
+		-- by default of 3... if load time is longer, then the announcement will show when screen is loaded
+		fftmain("a")
+	end)
+end)
+
+	
 --end--if you missed a close function, this will at least help format to track down the oops
